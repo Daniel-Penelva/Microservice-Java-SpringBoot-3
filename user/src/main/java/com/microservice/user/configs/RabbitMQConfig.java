8a5a3a8ce6a0ff6f3c30e75c90d1df59d4408bcb
Daniel Penelva <d4n.andrade@gmail.com>
2023-11-23 02:42:37 -0300

    Configuração do RabbitMQConfig + configurações do rabbitMQ no user (application.properties)

---
+++ user/src/main/java/com/microservice/user/configs/RabbitMQConfig.java
@@ -0,0 +1,19 @@
+package com.microservice.user.configs;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    /**
+     * Esse método será usada para converter objetos Java em mensagens JSON ao enviar mensagens para RabbitMQ e vice-versa ao receber mensagens da fila.*/
+    @Bean
+    public Jackson2JsonMessageConverter messageConverter(){
+        ObjectMapper objectMapper = new ObjectMapper();
+
+        return new Jackson2JsonMessageConverter(objectMapper);
+    }
+}
