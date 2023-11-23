f4078c34269fa8fb89c2848871f4dba358a6c2b1
Daniel Penelva <d4n.andrade@gmail.com>
2023-11-23 19:56:01 -0300

    Implementando EmailConsumer + configuração das propriedades do email (application.properties) para implementação de envio de emails com SMTP Gmail

--- email/src/main/java/com/microservice/email/consumers/EmailConsumer.java
+++ email/src/main/java/com/microservice/email/consumers/EmailConsumer.java
@@ -1,7 +1,9 @@
 package com.microservice.email.consumers;
 
 import com.microservice.email.dtos.EmailRecordDto;
+import com.microservice.email.models.EmailModel;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.BeanUtils;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
@@ -10,6 +12,12 @@ public class EmailConsumer {
 
     @RabbitListener(queues = "${broker.queue.email.name}")
     public void listenEmailQueue(@Payload EmailRecordDto emailRecordDto){
-        System.out.println(emailRecordDto.emailTo());
+
+        //System.out.println(emailRecordDto.emailTo());
+
+        var emailModel = new EmailModel();
+
+        // Converter o emailRecordDto para emailModel
+        BeanUtils.copyProperties(emailRecordDto, emailModel);
     }
 }
